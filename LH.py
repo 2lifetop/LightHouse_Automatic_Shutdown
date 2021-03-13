b4fecd8c86c427502ef450d985be931a1821424f
2lifetop <jiawen14th@gmail.com>
2021-03-13 20:29:48 +0800

    添加主程序，完善readme，添加初始log文件，添加默认参数

---
+++ LH.py
@@ -0,0 +1,58 @@
+import json
+from tencentcloud.common import credential
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+from tencentcloud.lighthouse.v20200324 import lighthouse_client, models
+try:
+    #参数
+    SecretId=""
+    SecretKey=""
+    region=""
+    percent= 0.95
+
+    # 以下不用管
+    cred = credential.Credential(SecretId,SecretKey)
+    httpProfile = HttpProfile()
+    httpProfile.endpoint = "lighthouse.tencentcloudapi.com"
+
+    clientProfile = ClientProfile()
+    clientProfile.httpProfile = httpProfile
+    client = lighthouse_client.LighthouseClient(cred, region, clientProfile)
+    #获取实例列表
+    req_instances = models.DescribeInstancesRequest()
+    params = {
+
+    }
+    req_instances.from_json_string(json.dumps(params))
+    resp_instances = client.DescribeInstances(req_instances) 
+    s1=json.loads(resp_instances.to_json_string())['InstanceSet']
+    for j in range (len(s1)):
+        params.setdefault("InstanceIds",[]).append(s1[j]['InstanceId'])
+
+    #获取实例流量
+    req = models.DescribeInstancesTrafficPackagesRequest()
+    req.from_json_string(json.dumps(params))  
+    resp = client.DescribeInstancesTrafficPackages(req)
+    s2=json.loads(resp.to_json_string())["InstanceTrafficPackageSet"]
+    GB=1024*1024*1024
+    for i in range (len(s2)):
+        InstanceId= s2[i]['InstanceId']
+        s3= s2[i]['TrafficPackageSet'][0]          
+        print (i+1,"：",InstanceId,":","已使用：",round(s3['TrafficUsed']/GB,2),"总流量：",round(s3['TrafficPackageTotal']/GB,2),"剩余：",round(s3['TrafficPackageRemaining']/GB,2))
+        #实例流量超出限制自动关闭
+
+        if (round(s3['TrafficUsed']/GB,2)/round(s3['TrafficPackageTotal']/GB,2)>percent):
+            print("剩余流量充足")
+        else:
+            print(InstanceId,":","流量超出限制，自动关闭")
+            req_Stop = models.StopInstancesRequest()
+            params_Stop = {
+
+            }
+            params_Stop.setdefault("InstanceIds",[]).append(InstanceId)
+            req_Stop.from_json_string(json.dumps(params_Stop))
+            resp_Stop = client.StopInstances(req_Stop) 
+            print(resp_Stop.to_json_string()) 
+except TencentCloudSDKException as err: 
+    print(err) 
--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # LightHouse_Automatic_Shutdown
 腾讯云轻量服务流量超出限制自动关机
+## 使用方法
+### 安装腾讯云Python SDK 3.0
+```
+pip3 install tencentcloud-sdk-python #python3
+```
+### 参数
+
+```
+    SecretId=""
+    SecretKey=""
+    region=""
+    percent= 0.95
+```
+
+SecretId,SecretKey 请前往腾讯云访问管理控制台获取：https://console.cloud.tencent.com/cam/capi
+
+![](https://img.jpggod.com/file/jpggod/2021/03/13/0b27e56b61dc83fcb881dc39a2747e8d.png)
+
+region 为服务器所在地域具体参照下表
+
+| 华北地区(北京)       | ap-beijing       |
+| -------------------- | ---------------- |
+| 西南地区(成都)       | ap-chengdu       |
+| 华南地区(广州)       | ap-guangzhou     |
+| 港澳台地区(中国香港) | ap-hongkong      |
+| 华东地区(南京)       | ap-nanjing       |
+| 华东地区(上海)       | ap-shanghai      |
+| 亚太东南(新加坡)     | ap-singapore     |
+| 亚太东北(东京)       | ap-tokyo         |
+| 欧洲地区(莫斯科)     | eu-moscow        |
+| 美国西部(硅谷)       | na-siliconvalley |
+
+[官方文档](https://cloud.tencent.com/document/product/1207/47564#.E5.9C.B0.E5.9F.9F.E5.88.97.E8.A1.A8)
+
+percent为 套餐内已使用流量占比 默认设置为 0.95，超出后自动关机
+
+建议搭配crontab使用
+
+如设置每个小时15分运行一次程序
+
+```
+crontab -e
+15 * * * * /usr/bin/python3 /root/LightHouse_Automatic_Shutdown/LH.py >> /root/LightHouse_Automatic_Shutdown/LH.log 2>&1
+```
+
+## 输出
+
+![](https://img.jpggod.com/file/jpggod/2021/03/13/4a104b8c8594f54b5a6273ed2249b088.png)
+
+## 更多功能
+
+- [ ] GitHub Actions版
+- [ ] 腾讯云函数版
+- [ ] server酱推送
+- [ ] TG推送
